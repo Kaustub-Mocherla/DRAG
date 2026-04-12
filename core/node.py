f020f43ae20f842bb232bd795ac0ae45c8927981
Arch-Kaustub <mocherla.kaustub@gmail.com>
2026-04-12 19:11:54 +0530

    node added

---
+++ core/node.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import chromadb
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+import os
+db_path = os.getenv("DB_PATH", "./chroma_db")
+app = FastAPI()
+
+
+embedding_function = SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+chroma_client = chromadb.PersistentClient(path=db_path)
+collection = chroma_client.get_collection("my_collection", embedding_function=embedding_function)
+
+class QueryRequest(BaseModel):
+    query: str
+
+@app.post("/query")
+def get_res(request: QueryRequest):
+    results = collection.query(query_texts=[request.query], n_results=10)
+    return {"chunks": results["documents"][0], "metadatas": results["metadatas"][0]}
